e1f745072daa8323dd08b9507e777958493ba0f7
william-s-s <williamschmidtsoares@gmail.com>
2024-02-05 01:46:27 -0300

    feat: TextToMIDI user interface

---
+++ FINAL_ASSIGNMENT/TextToMIDI.py
@@ -0,0 +1,105 @@
+import PySimpleGUI as gi
+from MIDICreator import midi_creator
+
+gi.theme('Black')
+
+title = [
+    gi.Text("TextToMIDI", font=("System", 32))
+]
+
+column1 = [
+    [
+        gi.Text("Text file:", font=("System", 12)),
+        gi.In(size=(50, 1), enable_events=True, key="text_file_path"),
+        gi.FileBrowse(file_types=(("Text Files", "*.txt"),))
+    ],
+    [
+        gi.Button('Read file', button_color=('white', 'firebrick3'), key="read_file")
+    ],
+    [
+        gi.Multiline(size=(70, 30), key='textbox')
+    ]
+]
+
+column2 = [
+    [
+        gi.Text("Generate MIDI file", font=("System", 24)),
+    ],
+    [
+        gi.Text("Folder to save file:", font=("System", 12)),
+        gi.In(size=(50, 1), enable_events=True, key="midi_save_folder", default_text="./output"),
+        gi.FolderBrowse()
+    ],
+    [
+        gi.Text("File name:", font=("System", 12)),
+        gi.In(size=(50, 1), enable_events=True, key="midi_save_file", default_text="output"),
+    ],
+    [
+        gi.Button('Generate file', button_color=('white', 'firebrick3'), key="generate_file")
+    ],
+    [
+        gi.HSeparator()
+    ],
+    [
+        gi.Text("Reproduce MIDI file", font=("System", 24)),
+    ],
+    [
+        gi.Text("Midi file:", font=("System", 12)),
+        gi.In(size=(50, 1), enable_events=True, key="midi_read_file_path", default_text="./output/output"),
+        gi.FileBrowse(file_types=(("Midi files", "*.mid"),))
+    ],
+    [
+        gi.Button('Reproduce file', button_color=('white', 'firebrick3'), key="reproduce_file")
+    ],
+]
+
+layout = [
+    [
+        [
+            [title],[gi.HSeparator()]
+        ],
+            gi.Column(column1, vertical_alignment = 't'),
+            gi.VSeperator(),
+            gi.Column(column2, vertical_alignment = 't')
+    ]
+]
+window = gi.Window("TextToMIDI", layout)
+
+while True:
+    event, values = window.read()
+    if event == "read_file":
+        try:
+            with open(values['text_file_path'], 'r', encoding="utf-8") as text_file:
+                window['textbox'].update(value=values['textbox'] + text_file.read())
+        except:
+            gi.popup_ok("No file found")
+    elif event == "generate_file":
+        folder = values['midi_save_folder']
+        filename = values['midi_save_file']
+        text = values['textbox']
+        if folder == "":
+            gi.popup_ok("No folder specified")
+        elif filename == "":
+            gi.popup_ok("No filename specified")
+        elif text == "":
+            gi.popup_ok("No text has been given")
+        else:
+            if filename[-4:] != ".mid":
+                filename = filename + ".mid"
+            file_path = folder + "/" + filename
+            #try:
+            midi_creator.generateMIDIFile(file_path, text)
+            #except:
+            #    gi.popup_ok("Error: File didn't generate")
+            #else:
+            gi.popup_ok("File generated successfully")
+    elif event == "reproduce_file":
+        file_path = values['midi_read_file_path']
+        if file_path == "":
+            gi.popup_ok("No file found")
+        else:
+            pass
+    elif event == "OK" or event == gi.WIN_CLOSED:
+        break
+
+window.close()
